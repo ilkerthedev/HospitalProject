a538bca591974eaa65dcb518ae96058ba470aa9b
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-06 21:32:27 +0300

    Day 04

---
+++ src/business/abstracts/AppointmentService.java
@@ -0,0 +1,21 @@
+package business.abstracts;
+
+import entities.concretes.Appointment;
+
+import java.util.List;
+
+public interface AppointmentService {
+
+    Appointment createAppointment();
+
+    Appointment getAppointment(String id);
+
+    Appointment cancelAppointment(String id);
+
+    List<Appointment> getAllAppointmentsList();
+
+    List<Appointment> getCompletedAppointmentsList();
+
+    List<Appointment> getContinuingAppointmentsList();
+
+}
